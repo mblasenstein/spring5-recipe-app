9ccc97ce70ef525e01b6ff41c6b7a33f033e62d5
Michael Blasentein <mblasen@gmail.com>
2019-08-21 12:41:07 -0700

    Changed UoM string val from measure to description

--- src/main/java/guru/springframework/domain/UnitOfMeasure.java
+++ src/main/java/guru/springframework/domain/UnitOfMeasure.java
@@ -11,7 +11,7 @@ public class UnitOfMeasure {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private String measure;
+    private String description;
 
     public Long getId() {
         return id;
@@ -21,11 +21,11 @@ public class UnitOfMeasure {
         this.id = id;
     }
 
-    public String getMeasure() {
-        return measure;
+    public String getDescription() {
+        return description;
     }
 
-    public void setMeasure(String measure) {
-        this.measure = measure;
+    public void setDescription(String description) {
+        this.description = description;
     }
 }
