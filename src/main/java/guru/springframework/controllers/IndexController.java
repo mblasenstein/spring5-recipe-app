c31576053eb516419b289db2edabdff70cc82edb
Michael Blasentein <mblasen@gmail.com>
2019-08-21 13:42:33 -0700

    Added JPA query methods to IndexController

--- src/main/java/guru/springframework/controllers/IndexController.java
+++ src/main/java/guru/springframework/controllers/IndexController.java
@@ -1,7 +1,13 @@
 package guru.springframework.controllers;
 
+import guru.springframework.domain.Category;
+import guru.springframework.domain.UnitOfMeasure;
+import guru.springframework.repositories.CategoryRepository;
+import guru.springframework.repositories.UnitOfMeasureRespository;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Optional;
 
 /**
  * Created by jt on 6/1/17.
@@ -9,8 +15,23 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class IndexController {
 
+    private CategoryRepository categoryRepository;
+    private UnitOfMeasureRespository unitOfMeasureRespository;
+
+    public IndexController(CategoryRepository categoryRepository, UnitOfMeasureRespository unitOfMeasureRespository) {
+        this.categoryRepository = categoryRepository;
+        this.unitOfMeasureRespository = unitOfMeasureRespository;
+    }
+
     @RequestMapping({"", "/", "/index"})
     public String getIndexPage(){
+
+        Optional<Category> categoryOptional = categoryRepository.findByDescription("American");
+        Optional<UnitOfMeasure> unitOfMeasureOptional = unitOfMeasureRespository.findByDescription("Teaspoon");
+
+        System.out.println("Category ID is: " + categoryOptional.get().getId());
+        System.out.println("Unit of Measure ID is: " + unitOfMeasureOptional.get().getId());
+
         return "index";
     }
 }
